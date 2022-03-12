24e66e85cb86fda8ed9d376fc6f328f461dbe9d3
Chase A <mr.alexander5132@gmail.com>
2022-03-12 11:02:34 -0800

    Started playing with image reading/writing and registration

---
+++ main.py
@@ -0,0 +1,52 @@
+import SimpleITK as sitk
+import vtk as vtk
+import math
+import sys
+import os
+
+
+def command_iteration(method):
+    if (method.GetOptimizerIteration() == 0):
+        print("Estimated Scales: ", method.GetOptimizerScales())
+    print(f"{method.GetOptimizerIteration():3} = {method.GetMetricValue():7.5f} : {method.GetOptimizerPosition()}")
+
+# Check that all aargs were provided
+if len(sys.argv) < 4:
+    print("Usage:", sys.argv[0], "<fixedImageFile> <movingImageFile>  <outputTransformFile>")
+    sys.exit(1)
+
+# Read-in the two images passed as command line args 
+pixelType = sitk.sitkFloat32
+fixed = sitk.ReadImage(sys.argv[1], pixelType)
+moving = sitk.ReadImage(sys.argv[2], pixelType)
+
+R = sitk.ImageRegistrationMethod()
+R.SetMetricAsMeanSquares()
+R.SetOptimizerAsRegularStepGradientDescent(4.0, .01, 200)
+R.SetInitialTransform(sitk.TranslationTransform(fixed.GetDimension()))
+R.SetInterpolator(sitk.sitkLinear)
+
+R.AddCommand(sitk.sitkIterationEvent, lambda: command_iteration(R))
+
+outTx = R.Execute(fixed, moving)
+
+print("-------")
+print(outTx)
+print(f"Optimizer stop condition: {R.GetOptimizerStopConditionDescription()}")
+print(f" Iteration: {R.GetOptimizerIteration()}")
+print(f" Metric value: {R.GetMetricValue()}")
+
+sitk.WriteTransform(outTx, sys.argv[3])
+
+if ("SITK_NOSHOW" not in os.environ):
+    resampler = sitk.ResampleImageFilter()
+    resampler.SetReferenceImage(fixed)
+    resampler.SetInterpolator(sitk.sitkLinear)
+    resampler.SetDefaultPixelValue(100)
+    resampler.SetTransform(outTx)
+
+    out = resampler.Execute(moving)
+    simg1 = sitk.Cast(sitk.RescaleIntensity(fixed), sitk.sitkUInt8)
+    simg2 = sitk.Cast(sitk.RescaleIntensity(out), sitk.sitkUInt8)
+    cimg = sitk.Compose(simg1, simg2, simg1 // 2. + simg2 // 2.)
+    sitk.Show(cimg, "ImageRegistration1 Composition")
