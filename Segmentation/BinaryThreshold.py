a499285568a82a5a0f346299c3d39e1a616a9be1
wrattan <70415445+wrattan@users.noreply.github.com>
2022-04-18 13:16:16 -0700

    Updated from ITK to SimpleITK

--- Segmentation/BinaryThreshold.py
+++ Segmentation/BinaryThreshold.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 
-import itk
+import SimpleITK as sitk
 import argparse
+import os
 
 parser = argparse.ArgumentParser(description="Threshold An Image Using Binary.")
 parser.add_argument("input_image")
@@ -11,24 +12,20 @@ parser.add_argument("upper_threshold", type=int)
 
 args = parser.parse_args()
 
-PixelType = itk.UC
-Dimension = 3
+PixelType = sitk.sitkFloat32
 
-ImageType = itk.Image[PixelType, Dimension]
-
-reader = itk.ImageFileReader[ImageType].New()
+reader = sitk.ImageFileReader()
+reader.SetImageIO("NiftiImageIO")
 reader.SetFileName(args.input_image)
+image = reader.Execute()
 
-thresholdFilter = itk.BinaryThresholdImageFilter[ImageType, ImageType].New()
-thresholdFilter.SetInput(reader.GetOutput())
-
+thresholdFilter = sitk.BinaryThresholdImageFilter()
 thresholdFilter.SetLowerThreshold(args.lower_threshold)
 thresholdFilter.SetUpperThreshold(args.upper_threshold)
 thresholdFilter.SetOutsideValue(0)
 thresholdFilter.SetInsideValue(255)
+image = thresholdFilter.Execute(image)
 
-writer = itk.ImageFileWriter[ImageType].New()
+writer = sitk.ImageFileWriter()
 writer.SetFileName(args.output_image)
-writer.SetInput(thresholdFilter.GetOutput())
-
-writer.Update()
+writer.Execute(image)
