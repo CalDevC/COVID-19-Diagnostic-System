71712a0e93dd2b43eca1a52742426800c83f7c0a
wrattan <70415445+wrattan@users.noreply.github.com>
2022-05-07 19:51:25 -0700

    Added comments

--- Segmentation/BinaryThreshold.py
+++ Segmentation/BinaryThreshold.py
@@ -5,19 +5,24 @@ import os
 
 def binThreshold(input_image, output_image, lower_threshold, upper_threshold, inside_value, outside_value):
     PixelType = sitk.sitkFloat32
-
+    #Read the image that the threshold will be applied to
     reader = sitk.ImageFileReader()
     reader.SetImageIO("NiftiImageIO")
     reader.SetFileName(input_image)
     image = reader.Execute()
-
+    #Threshold the image 
     thresholdFilter = sitk.BinaryThresholdImageFilter()
+    #Lower Threshold is the lowest pixel value in the range of pixels that the threshold will be applied too
     thresholdFilter.SetLowerThreshold(lower_threshold)
+    #Upper Threshold is the highest pixel value in the range of pixels that the threshold will be applied too
     thresholdFilter.SetUpperThreshold(upper_threshold)
+    #Whatever pixels values that arent in the range of lower to upper threshold value will be set to this color value
     thresholdFilter.SetOutsideValue(outside_value)
+    #Whatever pixels values that are in the range of lower to upper threshold value will be set to this color value
     thresholdFilter.SetInsideValue(inside_value)
     image = thresholdFilter.Execute(image)
-
+    
+    #Write out a new image that has been thresholded 
     writer = sitk.ImageFileWriter()
     writer.SetFileName(output_image)
     writer.Execute(image)
