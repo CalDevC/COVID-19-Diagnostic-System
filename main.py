f718f67c92d3f3cabf73604148d710211ae07b77
Chase A <mr.alexander5132@gmail.com>
2022-05-07 19:40:56 -0700

    Added comments to main

--- main.py
+++ main.py
@@ -6,17 +6,17 @@ import SimpleITK as sitk
 from Segmentation.BilateralSmoothing import biLatSmooth
 from Segmentation.BinaryThreshold import binThreshold
 
-startingNegPatient = "images/healthyLung"
+# Paths to images/directories
+normalPatient = "images/healthyLung"
 normalPatientNIfTI = "images/healthy.nii"
 normalPatientSegNIfTI = "images/healthySeg.nii"
-
 testPatientNIfTI = "images/covidLung.nii"
-
 finalOutput = "images/final.nii"
 
+# Read the dICOM series of the normal patient and save it as a NIfTI file
 reader = sitk.ImageSeriesReader()
-print("Reading DICOM files from directory: ", startingNegPatient)
-dicom_names = reader.GetGDCMSeriesFileNames(startingNegPatient)
+print("Reading DICOM files from directory: ", normalPatient)
+dicom_names = reader.GetGDCMSeriesFileNames(normalPatient)
 reader.SetFileNames(dicom_names)
 sitk.WriteImage(reader.Execute(), normalPatientNIfTI)
 
@@ -29,13 +29,16 @@ sitk.WriteImage(reader.Execute(), normalPatientNIfTI)
 # sitk.WriteImage(reader.Execute(), testPatientNIfTI)
 
 print("Starting on Segmentation")
+# Perform segmentation on the healthy patient
 biLatSmooth(normalPatientNIfTI, normalPatientSegNIfTI)
 binThreshold(normalPatientSegNIfTI, normalPatientSegNIfTI, 80, 120, 100, 0)
 
+# Perform segmentation on the test patient
 biLatSmooth(testPatientNIfTI, finalOutput)
 binThreshold(finalOutput, finalOutput, 35, 45, 50, 0)
 print("Segmentation Complete!")
 
 print("Starting on Registration")
+# Register the 2 segmented images
 reg(normalPatientSegNIfTI, finalOutput, finalOutput)
 print("Registration Complete!")
